95030c12f905bff566cf73e534488c09db540b18
aamerkotbicode <aamerkotbi6924@gmail.com>
2026-07-30 09:12:58 +0300

    removes unnessary files

--- temp.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-
-func _process(delta):
-	if Input.is_action_pressed("ui_right"):
-		position.x += 10 * delta
-	if Input.is_action_pressed("ui_left"):
-		position.x -= 10 * delta
-	print(delta)
